117fb1cab43a1b5d62c9ffb6a6fb56be6326c318
Alshimysth <alshimysth@gmail.com>
2024-11-07 18:25:57 +0100

    initial commit

---
+++ src/main/java/com/example/online_cv_server/OnlineCvServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_cv_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCvServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCvServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/online_cv_server/OnlineCvServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_cv_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCvServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
